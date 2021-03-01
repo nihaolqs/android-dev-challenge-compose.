4d442b8f9c7b4f6f1fe54a119f87cb60082f14fe
linqingshan <linqingshan@jze100.com>
2021-03-01 15:34:39 +0800

    feat

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -15,17 +15,43 @@
  */
 package com.example.androiddevchallenge
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
+
+    val array = Array(100) {
+        Pet(
+            "Dog${it}",
+            "",
+            3,
+            "Yellow"
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -33,29 +59,90 @@ class MainActivity : AppCompatActivity() {
                 MyApp()
             }
         }
-    }
-}
 
-// Start building your app here!
-@Composable
-fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
     }
-}
 
-@Preview("Light Theme", widthDp = 360, heightDp = 640)
-@Composable
-fun LightPreview() {
-    MyTheme {
-        MyApp()
+    private val startActivityLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                it.data?.getStringExtra(PET_RESULT_KEY).let { id ->
+                    array.find { it.id == id }
+                }?.isAdopt?.value = true
+            }
+        }
+
+    // Start building your app here!
+    @Composable
+    fun MyApp() {
+        LazyColumn {
+            items(items = array) {
+                PetItemContent(pet = it)
+            }
+        }
     }
-}
 
-@Preview("Dark Theme", widthDp = 360, heightDp = 640)
-@Composable
-fun DarkPreview() {
-    MyTheme(darkTheme = true) {
-        MyApp()
+
+    @Preview("Light Theme", widthDp = 360, heightDp = 640)
+    @Composable
+    fun LightPreview() {
+        MyTheme {
+            MyApp()
+        }
+    }
+
+    @Preview("Dark Theme", widthDp = 360, heightDp = 640)
+    @Composable
+    fun DarkPreview() {
+        MyTheme(darkTheme = true) {
+            MyApp()
+        }
+    }
+
+    @Composable
+    fun PetItemContent(pet: Pet) {
+        var isAdopt = remember { mutableStateOf(pet.isAdopt.value) }
+        pet.isAdopt = isAdopt
+        Card(
+            modifier = Modifier
+                .padding(15.dp)
+                .fillMaxWidth()
+                .alpha(if (isAdopt.value) 0.3f else 1f)
+        ) {
+            Surface(modifier = Modifier.padding(16.dp)) {
+                Row() {
+                    val imageModifier = Modifier
+                        .height(50.dp)
+                        .width(50.dp)
+                        .fillMaxWidth()
+
+                    Image(
+                        painterResource(id = R.drawable.dog1), "",
+                        modifier = imageModifier,
+                        contentScale = ContentScale.Crop
+                    )
+
+                    Spacer(Modifier.width(16.dp))
+
+                    Column(modifier = Modifier.width(150.dp)) {
+                        Text(text = "Name: ${pet.name}")
+                        Divider(color = Color.Transparent, thickness = 5.dp)
+                        Text(text = "Age: ${pet.age}")
+                    }
+                    if (!isAdopt.value) {
+                        Button(onClick = {
+                            startActivityLauncher.launch(
+                                Intent(
+                                    this@MainActivity,
+                                    DetailyActivity::class.java
+                                ).apply {
+                                    putExtra(PET_KEY, pet)
+                                })
+                        }) {
+                            Text(text = "Go Adopt")
+                        }
+                    }
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/androiddevchallenge/Pet.kt
@@ -0,0 +1,22 @@
+package com.example.androiddevchallenge
+
+import android.os.Parcelable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import kotlinx.android.parcel.IgnoredOnParcel
+import kotlinx.android.parcel.Parcelize
+import kotlinx.android.parcel.RawValue
+import java.util.*
+
+@Parcelize
+data class Pet(
+    val name: String,
+    val breed: String,
+    val age: Int,
+    val color: String,
+    val id: String = UUID.randomUUID().toString()
+
+) : Parcelable{
+    @IgnoredOnParcel
+    var isAdopt: MutableState<Boolean> = mutableStateOf(false)
+}
